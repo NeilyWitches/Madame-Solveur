604afdb8af5409ac39be4255c98f612e8456ff9b
neil <neil.pandya.np@gmail.com>
2021-10-10 15:52:16 -0400

    render student

--- src/scripts/level_1_proper.js
+++ src/scripts/level_1_proper.js
@@ -5,7 +5,6 @@ class Level1Proper {
         this.canvas.setAttribute('id', 'level-1-proper');
         this.ctx = this.draw();
         this.renderLevel();
-        // this.renderBalls();
     }
 
     draw() {
@@ -16,6 +15,8 @@ class Level1Proper {
 
     renderLevel() {
         this.renderTitles();
+        this.renderBalls();
+        this.renderImages();
     }
 
     renderTitles() {
@@ -32,6 +33,35 @@ class Level1Proper {
         this.ctx.textBaseLine = "top";
         this.ctx.textAlign = "center";
         this.ctx.fillText(text_h2, this.canvas.width / 2, 80);
+    }
+
+    renderBalls() {
+        for (let i = 0; i < 9; i++) {
+            this.ctx.beginPath();
+            this.ctx.arc(100 + i * 30, 150, 10, 0, Math.PI * 2);
+            this.ctx.fillStyle = "gray";
+            this.ctx.fill();
+            this.ctx.stroke();
+            this.ctx.closePath();
+        }
+    }
+
+    renderImages() {
+        const that = this;
+        const student = new Image();
+        student.src = 'assets/student_lvl_1.jpg';
+        const student_scale_factor = 1 / 8;
+        student.addEventListener('load', function() {
+            that.ctx.drawImage(student, 450, 150, student.width * student_scale_factor, student.height * student_scale_factor);
+        }, false);
+        const student_cite = new Image();
+        student_cite.src = 'assets/student_cite.jpg';
+        const student_cite_scale_factor = 1 / 4;
+        student.addEventListener('load', function () {
+            console.log(student_cite.width);
+            console.log(student_cite.height);
+            that.ctx.drawImage(student_cite, 395, 375, student_cite.width * student_cite_scale_factor, student_cite.height * student_cite_scale_factor);
+        }, false);
     }
 
 }
