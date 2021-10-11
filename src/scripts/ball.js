d80d849887790ec2f34e154cfea1753382f9b21e
neil <neil.pandya.np@gmail.com>
2021-10-10 22:51:09 -0400

    Konva test successful.

--- src/scripts/ball.js
+++ src/scripts/ball.js
@@ -1,6 +1,74 @@
 class Ball {
-    constructor() {
+    constructor(i) {
         this.heavy = false;
+        this.handle = {
+            x: 100 + i * 30,
+            y: 150,
+            radius: 10
+        };
+        this.canvas = document.getElementById("level-1-proper");
+        this.ctx = this.canvas.getContext("2d");
+        this.renderBall();
+        this.dragDrop();
+    }
+
+    renderBall() {
+        // this.clearCircle(this.ctx, this.handle.x, this.handle.y, this.handle.radius);
+
+        this.ctx.clearRect(this.handle.x - 12, this.handle.y - 12, 2.5 * this.handle.radius, 2.5 * this.handle.radius);
+        this.ctx.beginPath();
+        this.ctx.arc(this.handle.x, this.handle.y, this.handle.radius, 0, Math.PI * 2);
+        this.ctx.fillStyle = "gray";
+        this.ctx.fill();
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+
+    clearCircle(ctx, x, y, radius) {
+        console.log("inside clearCircle");
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2*Math.PI, true);
+        ctx.clip();
+        ctx.clearRect(x - radius, y - radius, radius * 2, radius * 2);
+        ctx.restore();
+    }
+
+    circlePointCollision(x, y, circle) {
+        return this.distanceXY(x, y, circle.x, circle.y) < circle.radius;
+    }
+
+    distanceXY(x0, y0, x1, y1) {
+        const dx = x1 - x0;
+        const dy = y1 - y0;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    dragDrop() {
+        this.canvas.addEventListener("mousedown", (e) => {
+            const rect = this.canvas.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            if(this.circlePointCollision(x, y, this.handle)) {
+                console.log("inside circle");
+                this.canvas.addEventListener("mousemove", this.onMouseMove.bind(this));
+                this.canvas.addEventListener("mouseup", this.onMouseUp.bind(this));
+            }
+        });
+    }
+
+    onMouseMove(event) {
+        const rect = this.canvas.getBoundingClientRect();
+        this.handle.x = event.clientX - rect.left;
+        this.handle.y = event.clientY - rect.top;
+        this.renderBall();
+    }
+
+    onMouseUp() {
+        console.log("mouseup");
+        const that = this;
+        this.canvas.removeEventListener("mousemove", that.onMouseMove.bind(that));
+        this.canvas.removeEventListener("mouseup", that.onMouseUp.bind(that));
     }
 }
 
--- src/scripts/level_1_proper.js
+++ src/scripts/level_1_proper.js
@@ -1,73 +1,99 @@
-import Ball from './ball'
+// import Ball from './ball'
+
+import Konva from "konva";
+
+// class Level1Proper {
+//     constructor () {
+//         this.canvas = document.createElement('canvas');
+//         document.getElementById('body').appendChild(this.canvas);
+//         this.canvas.setAttribute('id', 'level-1-proper');
+//         this.ctx = this.draw();
+//         this.renderLevel();
+//     }
+
+//     draw() {
+//         this.canvas.setAttribute('width', '600');
+//         this.canvas.setAttribute('height', '700');
+//         return this.canvas.getContext('2d');
+//     }
+
+//     renderLevel() {
+//         this.renderTitles();
+//         this.renderBalls();
+//         this.renderImages();
+//     }
+
+//     renderTitles() {
+//         const text_h1 = "Level 1:";
+//         this.ctx.fillStyle = "black";
+//         this.ctx.font = "bold 45px Arial";
+//         this.ctx.textBaseline = "top";
+//         this.ctx.textAlign = "center";
+//         this.ctx.fillText(text_h1, this.canvas.width / 2, 25);
+
+//         const text_h2 = "Find the heavier ball bearing!";
+//         this.ctx.fillStyle = "purple";
+//         this.ctx.font = "bold 25px Arial";
+//         this.ctx.textBaseLine = "top";
+//         this.ctx.textAlign = "center";
+//         this.ctx.fillText(text_h2, this.canvas.width / 2, 80);
+//     }
+
+//     renderBalls() {
+//         // const balls = [];
+//         // for (let i = 0; i < 9; i++) {
+//         //     balls.push(new Ball(i));
+//         // }
+//         // balls[Math.floor(Math.random() * 9)].heavy = true;
+//         new Ball(0);
+//     }
+
+//     renderImages() {
+//         const that = this;
+//         const student = new Image();
+//         student.src = 'assets/student_lvl_1.jpg';
+//         const student_scale_factor = 1 / 8;
+//         student.addEventListener('load', function() {
+//             that.ctx.drawImage(student, 450, 150, student.width * student_scale_factor, student.height * student_scale_factor);
+//         }, false);
+//         const student_cite = new Image();
+//         student_cite.src = 'assets/student_cite.jpg';
+//         const student_cite_scale_factor = 1 / 4;
+//         student.addEventListener('load', function () {
+//             that.ctx.drawImage(student_cite, 395, 375, student_cite.width * student_cite_scale_factor, student_cite.height * student_cite_scale_factor);
+//         }, false);
+//     }
+
+// }
+
+// export default Level1Proper;
+
 
 class Level1Proper {
-    constructor () {
-        this.canvas = document.createElement('canvas');
-        document.getElementById('body').appendChild(this.canvas);
-        this.canvas.setAttribute('id', 'level-1-proper');
-        this.ctx = this.draw();
-        this.renderLevel();
+    constructor() {
+        this.stage = new Konva.Stage({
+            container: 'body',
+            width: 500,
+            height: 500
+        });
+
+        this.test();
     }
 
-    draw() {
-        this.canvas.setAttribute('width', '600');
-        this.canvas.setAttribute('height', '700');
-        return this.canvas.getContext('2d');
+    test() {
+        const layer = new Konva.Layer();
+        const circle = new Konva.Circle({
+            x: this.stage.width() / 2,
+            y: this.stage.height() / 2,
+            radius: 70,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4
+        });
+        layer.add(circle);
+        this.stage.add(layer);
+        layer.draw();
     }
-
-    renderLevel() {
-        this.renderTitles();
-        this.renderBalls();
-        this.renderImages();
-    }
-
-    renderTitles() {
-        const text_h1 = "Level 1:";
-        this.ctx.fillStyle = "black";
-        this.ctx.font = "bold 45px Arial";
-        this.ctx.textBaseline = "top";
-        this.ctx.textAlign = "center";
-        this.ctx.fillText(text_h1, this.canvas.width / 2, 25);
-
-        const text_h2 = "Find the heavier ball bearing!";
-        this.ctx.fillStyle = "purple";
-        this.ctx.font = "bold 25px Arial";
-        this.ctx.textBaseLine = "top";
-        this.ctx.textAlign = "center";
-        this.ctx.fillText(text_h2, this.canvas.width / 2, 80);
-    }
-
-    renderBalls() {
-        const balls = [];
-        for (let i = 0; i < 9; i++) {
-            balls.push(new Ball ());
-            this.ctx.beginPath();
-            this.ctx.arc(100 + i * 30, 150, 10, 0, Math.PI * 2);
-            this.ctx.fillStyle = "gray";
-            this.ctx.fill();
-            this.ctx.stroke();
-            this.ctx.closePath();
-        }
-        balls[Math.floor(Math.random() * 9)].heavy = true;
-        console.log(balls);
-    }
-
-    renderImages() {
-        const that = this;
-        const student = new Image();
-        student.src = 'assets/student_lvl_1.jpg';
-        const student_scale_factor = 1 / 8;
-        student.addEventListener('load', function() {
-            that.ctx.drawImage(student, 450, 150, student.width * student_scale_factor, student.height * student_scale_factor);
-        }, false);
-        const student_cite = new Image();
-        student_cite.src = 'assets/student_cite.jpg';
-        const student_cite_scale_factor = 1 / 4;
-        student.addEventListener('load', function () {
-            that.ctx.drawImage(student_cite, 395, 375, student_cite.width * student_cite_scale_factor, student_cite.height * student_cite_scale_factor);
-        }, false);
-    }
-
 }
 
 export default Level1Proper;
