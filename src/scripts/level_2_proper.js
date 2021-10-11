f1652600642bc136c0a9ac48a9a5e662c107c016
neil <neil.pandya.np@gmail.com>
2021-10-11 19:37:19 -0400

    Render level 2.

--- src/scripts/level_2_proper.js
+++ src/scripts/level_2_proper.js
@@ -21,23 +21,36 @@ class Level2Proper {
         const li_1 = document.createElement('li');
         ul.appendChild(li_1);
         li_1.setAttribute('id', 'stud-num-label');
+        li_1.setAttribute('class', 'student');
+        li_1.classList.add('label');
         li_1.innerText = "Student's Number:";
 
         const li_2 = document.createElement('li');
         ul.appendChild(li_2);
         li_2.setAttribute('id', 'stud-num');
-        li_2.innerText = "";
+        li_2.setAttribute('class', 'student');
+        li_2.classList.add('number');
+        li_2.innerText = "You go first!";
 
         const li_3 = document.createElement('li');
         ul.appendChild(li_3);
         li_3.setAttribute('id', 'mme-num-label');
+        li_3.setAttribute('class', "mme");
+        li_3.classList.add('label');
         li_3.innerText = "Mme Solveur's Number:";
 
         const li_4 = document.createElement('li');
         ul.appendChild(li_4);
+        li_4.setAttribute('id', "mme-num");
+        li_4.setAttribute('class', 'mme');
+        li_4.classList.add('number');
+        li_4.innerText = "Input a number from 1 to 10!";
+
+        const li_5 = document.createElement('li');
+        ul.appendChild(li_5);
         const input = document.createElement('input');
-        li_4.appendChild(input);
-        li_4.setAttribute('type', 'text');
+        li_5.appendChild(input);
+        li_5.setAttribute('type', 'text');
     }
 }
 
