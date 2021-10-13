acdeaa79cf9bed46fb5dc2382a9491b5a69e5c1d
neil <neil.pandya.np@gmail.com>
2021-10-13 16:45:41 -0400

    straight to level 3 added nav links.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import StartMenu from "./scripts/start_menu";
+// import StartMenu from "./scripts/start_menu";
+import Level1Instructions from "./scripts/level_1_instructions";
+import Level3 from "./scripts/level_3";
 
 document.addEventListener("DOMContentLoaded", () => {
-    new StartMenu();
+    // new Level3();
+    // new StartMenu();
+    // new Level3();
+    // new Level1Instructions();
+    new Level3();
 })
--- src/scripts/level_3.js
+++ src/scripts/level_3.js
@@ -1,16 +1,91 @@
+// import StartMenu from "./start_menu";
+
 class Level3 {
-    constructor () {
-        this.renderLevel();
-
+    constructor() {
+        this.screen = document.createElement('div');
+        document.getElementById('body').appendChild(this.screen);
+        this.screen.setAttribute('id', 'level-3');
+        this.renderScreen();
+        // this.clickStart();
     }
 
-    renderLevel() {
+    renderScreen() {
+        this.renderLinks();
+        // this.renderButton();
+    }
+
+    renderLinks() {
         const h1 = document.createElement('h1');
-        document.getElementById('body').appendChild(h1);
-        h1.innerText = 'More levels coming soon.'
+        this.screen.appendChild(h1);
+        h1.innerText = "Level 3:"
+
+        const h2 = document.createElement('h2');
+        h1.appendChild(h2);
+        h2.innerText = "More levels coming soon!"
+
+        const ul = document.createElement('ul');
+        this.screen.appendChild(ul);
+        ul.setAttribute('id', 'socials');
+
+        const li_1 = document.createElement('li');
+        li_1.setAttribute('id', 'li-1')
+        ul.appendChild(li_1);
+
+        const myGitHub = document.createElement('a');
+        myGitHub.setAttribute('href', 'https://github.com/NeilyWitches');
+        li_1.appendChild(myGitHub);
+
+        const gitHubIcon = new Image();
+        gitHubIcon.setAttribute('id', 'github');
+        myGitHub.appendChild(gitHubIcon);
+        gitHubIcon.onload = function () {
+            this.width = this.width;
+        }
+        gitHubIcon.src = "assets/github.png";
+
+        const myLinkedIn = document.createElement('a');
+        myLinkedIn.setAttribute('href', 'https://www.linkedin.com/in/neil-pandya-610588187/');
+        li_1.appendChild(myLinkedIn);
+
+        const linkedInIcon = new Image();
+        linkedInIcon.setAttribute('id', 'linkedin');
+        myLinkedIn.appendChild(linkedInIcon);
+        const scaleFactorLinkedIn = 1 / 5;
+        linkedInIcon.onload = function () {
+            this.width = this.width * scaleFactorLinkedIn;
+        }
+        linkedInIcon.src = 'assets/linkedin.png';
+
+        const h3 = document.createElement('h3');
+        li_1.appendChild(h3);
+        h3.innerText = "Attributions:"
     }
 
-    
+    renderButton() {
+        const li_3 = document.createElement('li');
+        const ul = document.getElementById('socials');
+        ul.appendChild(li_3);
+
+        const startButton = document.createElement('button');
+        li_3.appendChild(startButton);
+        startButton.setAttribute('id', 'start-button');
+        startButton.setAttribute('type', 'button');
+        startButton.innerText = "Start";
+    }
+
+    clickStart() {
+        const button = document.getElementById('start-button');
+        button.addEventListener('click', handleClick);
+
+        function handleClick(event) {
+            button.removeEventListener('click', handleClick);
+            const body = document.getElementById('body');
+            while (body.firstChild) {
+                body.removeChild(body.firstChild);
+            }
+            new Level1Instructions();
+        }
+    }
 }
 
 export default Level3;
