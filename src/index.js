7d9a6d926a397962a16761e8517cb5196a03a16f
neil <neil.pandya.np@gmail.com>
2021-10-14 12:00:56 -0400

    Load from beginning.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // import Level1Redo from "./scripts/level_1_redo";
 // import Level2Instructions from "./scripts/level_2";
-import Level3 from "./scripts/level_3";
-// import StartMenu from "./scripts/start_menu";
+// import Level3 from "./scripts/level_3";
+import StartMenu from "./scripts/start_menu";
 
 document.addEventListener("DOMContentLoaded", () => {
-    // new StartMenu();
+    new StartMenu();
     // new Level1Redo
     // new Level2Instructions;
-    new Level3;
+    // new Level3;
 })
