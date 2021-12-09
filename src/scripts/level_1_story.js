7ceaf1e89912595675ba8b801fc1296da8846241
neil <neil.pandya.np@gmail.com>
2021-12-09 17:08:23 -0500

    Refactor level 1 story next button

--- src/scripts/level_1_story.js
+++ src/scripts/level_1_story.js
@@ -3,9 +3,11 @@ import Level1Instructions from './level_1_instructions';
 class Level1Story {
     constructor() {
         this.screen = document.getElementById('screen')
+        this.clickNext = this.clickNext.bind(this);
+        this.clickSkip = this.clickSkip.bind(this);
+        this.clicked = 0;
         this.renderStory();
         this.clickNext();
-        this.clickSkip = this.clickSkip.bind(this);
     }
 
     renderStory() {
@@ -45,6 +47,7 @@ class Level1Story {
         this.screen.appendChild(this.nextButton);
         this.nextButton.setAttribute('id', 'next-button');
         this.nextButton.innerText = "Next";
+        this.nextButton.addEventListener('click', this.clickNext);
     }
 
     clickSkip() {
@@ -64,52 +67,44 @@ class Level1Story {
     }
 
     clickNext() {
-        let clicked = 0;
-        const that = this;
-        this.nextButton.addEventListener('click', handleClick);
-
-        function handleClick() {
+        this.clicked += 1
+        if (this.clicked === 1) {
             
-            clicked += 1
-            if (clicked === 1) {
-                
-                that.dialogue.innerText = 'As she thought about a possible candidate for dark matter, a freshman had approached her office knocking on the open door. '
-                
-                const student = new Image();
-                student.setAttribute('id', 'student-story');
-                that.screen.appendChild(student);
-                student.src = "assets/student_needs_help.png";
-                let pos = 580;
-                let studentKnocks = setInterval(moveStudent, 10);
-                
-                function moveStudent() {
-                    if (pos == 350) {
-                        clearInterval(studentKnocks);
-                    } else {
-                        pos--;
-                        student.style.left = pos + 'px';
-                    }
+            this.dialogue.innerText = 'As she thought about a possible candidate for dark matter, a freshman had approached her office knocking on the open door. '
+            
+            const student = new Image();
+            student.setAttribute('id', 'student-story');
+            this.screen.appendChild(student);
+            student.src = "assets/student_needs_help.png";
+            let pos = 580;
+            let studentKnocks = setInterval(moveStudent, 10);
+            
+            function moveStudent() {
+                if (pos == 350) {
+                    clearInterval(studentKnocks);
+                } else {
+                    pos--;
+                    student.style.left = pos + 'px';
                 }
+            }
 
-            }
-            else if (clicked === 2) {
-                that.dialogue.innerText = "Although startled, the physicist warmly beckoned them in and the student explained their reason for coming."             }
-            else if (clicked === 3) {
-                that.dialogue.innerText = "They were tasked with assisting in an experiment with one of Mme Solveur's colleagues and needed help with a problem they had encountered."
-            }
-            else if (clicked === 4) {
-                that.dialogue.innerText = "They heard about Mme Solveur's exceptional ability to solve puzzles..."
-            }
-            else if (clicked === 5) {
-                that.nextButton.removeEventListener('click', handleClick);
-                while (that.screen.firstChild) {
-                    that.screen.removeChild(that.screen.firstChild);
-                 }
-                new Level1Instructions();
-            }
+        }
+        else if (this.clicked === 2) {
+            this.dialogue.innerText = "Although startled, the physicist warmly beckoned them in and the student explained their reason for coming."             }
+        else if (this.clicked === 3) {
+            this.dialogue.innerText = "They were tasked with assisting in an experiment with one of Mme Solveur's colleagues and needed help with a problem they had encountered."
+        }
+        else if (this.clicked === 4) {
+            this.dialogue.innerText = "They heard about Mme Solveur's exceptional ability to solve puzzles..."
+        }
+        else if (this.clicked === 5) {
+            this.nextButton.removeEventListener('click', this.clickNext);
+            while (this.screen.firstChild) {
+                this.screen.removeChild(this.screen.firstChild);
+                }
+            new Level1Instructions();
         }
     }
-
 }
 
 export default Level1Story;
